0ea4a638b24d61427edae49da9959420c3b5cf1e
huavcjj <huavcjj0217@icloud.com>
2025-01-28 20:42:13 +0900

    feat

--- services/auth_service.go
+++ services/auth_service.go
@@ -13,6 +13,7 @@ import (
 type IAuthService interface {
 	Signup(email string, password string) error
 	Login(email, password string) (*string, error)
+	GetUserFromToken(tokenString string) (*models.User, error)
 }
 
 type authService struct {
@@ -62,4 +63,27 @@ func CreateToken(userId uint, email string) (*string, error) {
 		return nil, err
 	}
 	return &tokenString, nil
+}
+
+func (s *authService) GetUserFromToken(tokenString string) (*models.User, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	var user *models.User
+	if claims, ok := token.Claims.(jwt.MapClaims); ok {
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			return nil, jwt.ErrTokenExpired
+		}
+		user, err = s.repository.FindUserByEmail(claims["email"].(string))
+		if err != nil {
+			return nil, err
+		}
+	}
+	return user, nil
 }
